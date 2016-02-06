e0a0db4fed743789e88697d6d0938e14f299efa9
masazdream <masaz.dream@gmail.com>
2016-02-06 23:32:29 +0900

    レンタカー問題の定数

---
+++ rental_car_util.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+class RentalCarUtil():
+    MAX_NUM_RENTAL_CAR = 20
+
+    MAX_NUM_MOVE = 5
+
+if __name__ == '__main__':
+    rcu = RentalCarUtil()
+    print rcu.MAX_NUM_RENTAL_CAR
+    print rcu.MAX_NUM_MOVE
