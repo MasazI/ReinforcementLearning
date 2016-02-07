a2f055babc64c852149a42bdbd12ac7e7338179e
masazdream <masaz.dream@gmail.com>
2016-02-07 22:23:22 +0900

    割引率を外出し

--- rental_car_util.py
+++ rental_car_util.py
@@ -5,6 +5,10 @@ class RentalCarUtil():
 
     MAX_NUM_MOVE = 5
 
+    discount_rate = 0.9
+
+    delta_max = 0.05
+
 if __name__ == '__main__':
     rcu = RentalCarUtil()
     print rcu.MAX_NUM_RENTAL_CAR
