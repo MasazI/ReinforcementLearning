068cf56928936af8ec72c43a1fef65d0fd355c2c
masazdream <masaz.dream@gmail.com>
2016-02-16 01:40:56 +0900

    ブラックジャッククラスにプレイヤーとディーラの操作を追加(作業中)

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,7 @@
 # encoding: utf-8
+
+import random
+
 '''
 BlackJackクラス
 '''
@@ -25,10 +28,71 @@ class Blackjack:
     }
 
     def __init__(self):
-        self.player_cards = {}
+        self.player_cards = []
         self.player_total = 0
         self.player_ace = 0
 
-        self.dealer_cards = {}
+        self.dealer_cards = []
         self.dealer_total = 0
-        self.dealer_ace = 0 
+        self.dealer_ace = 0
+        
+        self.finish = False
+
+        # プレイヤーの初期化(控除率を上げるため11以下の場合は強制的にヒット)
+        while(self.player_total < 12):
+            self.player_hit()
+
+        # ディーラーの初期化(2枚のカードを引く)
+        for i in xrange(2):
+            self.dealer_draw()
+
+    def draw_card(self):
+        '''
+        カードをランダムに1枚引く
+        '''
+        return random.choice(self.CARD)
+
+    def player_draw(self):
+        '''
+        プレイヤーがカードを1枚引く
+        '''
+        card = self.draw_card()
+        self.player_cards.append(card)
+        if card == 'A':
+            self.player_ace += 1
+        self.player_total += self.CARD_VALUE[card]
+        if (self.player_total > 21) & (self.player_ace > 0):
+            self.player_total -= 10
+            self.player_ace -= 1
+
+
+    def dealer_draw(self):
+        '''
+        ディーラーがカードを1枚引く
+        '''
+        card = self.draw_card()
+        self.dealer_cards.append(card)
+        if card == 'A':
+            self.dealer_ace += 1
+        self.dealer_total += self.CARD_VALUE[card]
+        if (self.dealer_total > 21) & (self.dealer_ace > 0):
+            self.dealer_total -= 10
+            self.dealer_ace -= 1
+
+    def dealer_keep_draw(self):
+        '''
+        ディーラーが17以上になるまでカードを引く
+        '''
+        while(self.dealer_total < 17):
+            self.dealer_draw()
+
+def test():
+    blackjack = Blackjack() 
+    print blackjack.player_cards   
+    print blackjack.dealer_cards
+
+if __name__ == '__main__':
+    test()
+
+
+
