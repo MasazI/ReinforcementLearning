ccb7e0548232ffeb73419cc34256b85b1e79ad9c
masazdream <masaz.dream@gmail.com>
2016-03-13 00:29:59 +0900

    モンテカルロES法に1ゲーム実施した後の行動価値の更新と方策の更新を追加

--- mces.py
+++ mces.py
@@ -3,11 +3,17 @@
 モンテカルロES法クラス
 '''
 
-import blackjack
-import blackjack_action_value
-import blackjack_policy
+from blackjack import Blackjack
+from blackjack_action_value import ActionValue
+from blackjack_policy import Policy
+
+from collections import deque
+import random
 
 class MCES:
+    '''
+    モンテカルロES
+    '''
     def __init__(self, action_value, policy):
         '''
         初期化
@@ -18,5 +24,80 @@ class MCES:
         self.action_value = action_value
         self.policy = policy
 
+    def simulate(self, verbose=False):
+        '''
+        シュミレーション(ゲームを1回実行して価値観数と方策を更新する)
+        '''
+        game = Blackjack()
+        if verbose:
+            print("[blackjack simulate]:")
+            game.output()
+            print game.dealer_face_value()
 
-        
+        player_total_queue = deque()
+        player_has_ace_queue = deque()
+        player_hit_queue = deque()
+        dealer_face_value = game.dealer_face_value()
+
+        while(True):
+            player_total = game.player_total
+            player_has_ace = game.player_has_ace()
+
+            # 最初の行動はExploring Starts(ランダムに選択)
+            if not player_hit_queue:
+                player_hit = random.choice([True, False])
+            else:
+                # 方策によって行動を選択
+                player_hit = self.policy.hit(player_total, player_has_ace, dealer_face_value)
+
+            # キューに追加
+            player_total_queue.append(player_total)
+            player_has_ace_queue.append(player_has_ace)
+            player_hit_queue.append(player_hit)
+
+            if player_hit:
+                game.player_hit()
+                if verbose:
+                    game.output()
+                if game.finish:
+                    break
+            else:
+                game.player_stand()
+                if verbose:
+                    game.output()
+                break
+
+        # 勝敗を確認(報酬)
+        result = game.judgement_result()
+        if verbose:
+            print("result: %s" % (result))
+
+        # 今回の行動、状態、報酬によって行動価値を更新
+        for player_total, player_has_ace, player_hit in zip(player_total_queue, player_has_ace_queue, player_hit_queue):
+            self.action_value.update(player_total, player_has_ace, dealer_face_value, player_hit, result)
+
+        # 方策を更新（1ゲームごとに更新する）
+        for player_total, player_has_ace in zip(player_total_queue, player_has_ace_queue):
+            # 今回の状態でヒットした場合の価値
+            hit_value = self.action_value.get(player_total, player_has_ace, dealer_face_value, True)
+            # 今回の状態でスタンドした場合の価値
+            stand_value = self.action_value.get(player_total, player_has_ace, dealer_face_value, False)
+
+            if hit_value > stand_value:
+                # ヒットの価値が高ければ方策をヒットに更新
+                self.policy.set(player_total, player_has_ace, dealer_face_value, True)
+            elif hit_value < stand_value:
+                # ヒットの価値が低ければ方策をスタンドに更新
+                self.policy.set(player_total, player_has_ace, dealer_face_value, False)
+
+def test():
+    action_value = ActionValue()
+    policy = Policy()
+    mces = MCES(action_value, policy)
+    mces.simulate(verbose=True)
+    mces.action_value.output()
+    mces.policy.output()
+
+
+if __name__ == '__main__':
+    test()
