6e5361b3b7bc6802da9063c760e71d3784a18526
MasazI <masaz.dream@gmail.com>
2016-03-19 01:14:53 +0900

    カーレースを強化学習するためのレース用部品の作成

---
+++ race.py
@@ -0,0 +1,45 @@
+# encoding:utf-8
+'''
+レースコース用の定義ファイル
+'''
+
+# parts
+START = 'S'
+IN = '.'
+OUT = 'o'
+GOAL = 'G'
+
+# speed
+MAX_SPEED = 4
+
+# course
+COURSE1 = '''\
+oooooooooooo
+oooooooooooo
+oooooooooooo
+oo........Go
+oo........Go
+ooo.......Go
+oooo......Go
+oo......oooo
+o....ooooooo
+oo...ooooooo
+oo....oooooo
+oo......oooo
+ooo......ooo
+oooo......oo
+ooooo.....oo
+oooooo....oo
+ooooooo....o
+oooooo....oo
+oooo......oo
+oooo......oo
+oooo....oooo
+oooo..oooooo
+oo.....ooooo
+oo.....ooooo
+o.......oooo
+ooSSSSSooooo
+oooooooooooo
+oooooooooooo
+oooooooooooo'''
