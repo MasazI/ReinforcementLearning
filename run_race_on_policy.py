28b95d9c17a176ce705945ee674c069c42516c0c
MasazI <masaz.dream@gmail.com>
2016-03-21 10:51:13 +0900

    refs RDFF/#RDFF-55
    Explain: カーレースの方策ONの出力メッセージを追加

---
+++ run_race_on_policy.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+'''
+カーレース学習クラス
+'''
+
+import race
+from course import Course
+from race_action_value import RaceActionValue
+from race_policy import RacePolicy
+
+from race_mc_on_policy import MCONPRace
+
+def do(course_info):
+    '''
+    指定したコースについて学習する
+    arguments:
+        course_info: コース情報(文字列)
+    '''
+    course = Course(course_info)
+    value = RaceActionValue()
+    policy = RacePolicy(course)
+    mcon_policy_race = MCONPRace(course, value, policy, 0.3)
+
+    epochs = 30
+    iterations = 10000
+
+    for i in xrange(epochs):
+        print("*"*20)
+        print("epocs.%d" %(i))
+        for j in xrange(iterations):
+            mcon_policy_race.simulate(training=True, verbose=False)
+
+        print("="*20)
+        print("iteration.%d done" %(i*iterations))
+        for start in course.starts:
+            print("-"*25)
+            print("start: %s" % (start))
+            mcon_policy_race.simulate(start, training=False, verbose=True)
+
+
+if __name__ == '__main__':
+    do(race.COURSE1)
