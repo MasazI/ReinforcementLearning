16c3236d71d0dd4f5e9f7887ce158e3ec5436a4d
masazdream <masaz.dream@gmail.com>
2016-03-29 22:01:38 +0900

    盤面の状態クラスを追加

---
+++ tic_tac_toe_state.py
@@ -0,0 +1,119 @@
+#encoding: utf-8
+'''
+state of tic tac toe game
+'''
+from mark import Mark
+from maru_mark import Maru
+from batsu_mark import Batsu
+from empty_mark import Empty
+
+import copy
+
+class State:
+    win_state = ([0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6])
+
+    '''
+    状態クラス    
+    '''
+    def __init__(self):
+        # 盤面を配列で保持
+        self.state = [Mark(Empty())]*9
+
+    def get_valid_actions(self):
+        '''
+        選択可能な行動の取得
+        return:
+            空のマスを示すindex
+        '''
+        valid_actions = []
+        for i in xrange(9):
+            if self.state[i].is_empty():
+                valid_actions.append(i)
+        return valid_actions
+
+    def get(self, index):
+        '''
+        指定したindexのマスのMarkを返す
+        arguments:
+            index: インデックス
+        return:
+            インデックスのマスのMark
+        '''
+        return self.state[index]
+    
+    def set(self, index, mark):
+        '''
+        現在の状態を変えずに新しい状態を取得
+        arguments:
+            index: 状態を変えるマスのインデックス
+            mark: 状態を変えるマスのMark
+        return:
+            新しい状態
+        '''
+        new_state = State()
+        new_state.state = copy.deepcopy(self.state)
+        new_state.state[index] = mark
+        return new_state
+
+    def to_array(self):
+        '''
+        整数型の状態配列
+        '''
+        int_array = []
+        for mark in self.state:
+            int_array.append(mark.to_int())
+        return int_array
+
+    def is_win(self, mark):
+        '''
+        指定したマークの手が勝利かどうか
+        '''
+        mark_int = mark.to_int()
+        for i, j, k in self.win_state:
+            if self.state[i].to_int() == mark_int and self.state[j].to_int() == mark_int and self.state[k].to_int() == mark_int:
+                return True
+        return False
+
+    def is_draw(self):
+        if len(self.get_valid_actions()) == 0 and not self.is_win(Mark(Maru())) and not self.is_win(Mark(Batsu())):
+            return True
+        return False
+
+    def is_end(self):
+        if self.is_win(Mark(Maru())) or self.is_win(Mark(Batsu())) or len(self.get_valid_actions()) == 0:
+            return True
+        return False
+
+if __name__ == '__main__':
+    state1 = State()
+    print state1.state
+    print state1.get_valid_actions()
+    print state1.get(0).to_int()
+    new_state = state1.set(0, Mark(Maru()))
+    print new_state
+    print new_state.get(0).to_int()
+    print state1.to_array()
+    print new_state.to_array()
+    print state1.win_state
+    print state1.is_win(Mark(Maru()))
+    maru1 = Mark(Maru())
+    maru2 = Mark(Maru())
+    print maru1.to_int() == maru2.to_int()
+    new_state2 = new_state.set(1, Mark(Maru()))
+    new_state3 = new_state2.set(2, Mark(Maru()))
+    print new_state3.to_array()
+    print new_state3.is_win(Mark(Maru()))
+    print new_state2.is_draw()
+    print new_state2.is_end()
+    print new_state3.is_end()
+    new_state = state1.set(0, Mark(Batsu()))
+    new_state2 = new_state.set(1, Mark(Maru()))
+    new_state4 = new_state2.set(2, Mark(Batsu()))
+    new_state5 = new_state4.set(3, Mark(Maru()))
+    new_state6 = new_state5.set(4, Mark(Batsu()))
+    new_state7 = new_state6.set(5, Mark(Batsu()))
+    new_state8 = new_state7.set(6, Mark(Maru()))
+    new_state9 = new_state8.set(7, Mark(Batsu()))
+    new_state10 = new_state9.set(8, Mark(Maru()))
+    print new_state10.to_array()
+    print new_state10.is_draw()
